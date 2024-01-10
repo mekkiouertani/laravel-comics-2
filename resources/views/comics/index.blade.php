84c3c7a605422a4f831acb4767b9dd1a88f314f6
Mekki Ouertani <140953094+mekkiouertani@users.noreply.github.com>
2024-01-10 16:34:52 +0100

    updated styles

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -10,15 +10,21 @@
             @endif
             <div>
                 {{--  <a href="{{ route('comics.create') }}" class="btn btn-primary">Add </a> --}}
-                <form action="{{ route('comics.index') }}" method="GET">
-                    <select name="search" id="search" class="form-select mb-2">
-                        <option value="">Tutti gli elementi</option>
-                        <option value="comic book">Comic</option>
-                        <option value="graphic novel">Graphic</option>
-                        <option value="fumetto">Fumetto</option>
-                    </select>
-                    <button type="submit" class="btn btn-success">Cerca</button>
-                </form>
+                <section class="d-flex justify-content-between align-items-center  ">
+                    <form action="{{ route('comics.index') }}" method="GET">
+                        <h2>Seleziona il tipo di fumetto</h2>
+                        <select name="search" id="search" class="form-select mb-2 w-50">
+                            <option value="">Tutti gli elementi</option>
+                            <option value="comic book">Comic</option>
+                            <option value="graphic novel">Graphic</option>
+                            <option value="fumetto">Fumetto</option>
+                        </select>
+                        <button type="submit" class="btn btn-success">Cerca</button>
+                    </form>
+                    <div>
+                        <h2>DC SUPERHEROES</h2>
+                    </div>
+                </section>
                 {{-- <form action="{{ route('comics.index') }}" method="GET">
                     <input type="text" placeholder="Cerca un fumetto" class="form-control mb-2">
                     <button type="submit" class="btn btn-success">Cerca</button>
